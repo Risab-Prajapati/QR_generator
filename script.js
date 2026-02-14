cde91b78face8dddb9fd33b8c98b686469fe2627
Rishav Prajapati <rishavp063@gmail.com>
2026-02-14 15:36:09 +0530

    code modified

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ let image = document.querySelector("#qrimg");
 let qrImg = document.querySelector(".qr-img");
 
 function generateQR() {
-  image.src =
-    "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data= " +
-    input.value;
-  qrImg.style.display = "block";
+  if (input.value < 1) {
+    alert("Input field must be filled");
+    qrImg.style.display = "none";
+  } else {
+    image.src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data= " +
+      input.value;
+    qrImg.style.display = "block";
+  }
 }
 
 button.addEventListener("click", generateQR);
